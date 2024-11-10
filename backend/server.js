931e728484d03c8cb602917b73782a4d09f8b75e
kumarbaberwal <niteshkumarghn1@gmail.com>
2024-11-11 00:29:21 +0530

    server created

---
+++ backend/server.js
@@ -0,0 +1,18 @@
+import express from 'express';
+import { ENV_VARS } from './config/envVars.js';
+
+
+const app = express();
+app.use(express.json());
+
+const PORT = ENV_VARS.PORT;
+
+app.use('/', (req, res)=>{
+    res.json({
+        message: "Hello Kumar"
+    });
+});
+
+app.listen(PORT, ()=>{
+    console.log("Server started at http://localhost:" + PORT);
+});
